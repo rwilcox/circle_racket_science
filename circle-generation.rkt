405ba579ceeef694d4b606815c5ba637c06170c4
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-08-30 21:43:37 -0400

    initial commit / racket Circle generation script

---
+++ circle-generation.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(require yaml)
+
+(define (for-yaml d)
+  (display (yaml->string d #:style 'block ))  ; #:scalar-style '#\|)) <-- every scalar, not just long strings!
+ )
+
+(for-yaml
+
+; do not use #hash() here as it is only literal and will quote_everything_. And the literal DSL kinda sucks....
+(hash
+  "version" 2
+  "orbs"   (hash "node" "circleci/node@5.0.1")
+  "jobs" (hash
+        "build" (hash
+            "executor" "default"
+            "steps" "checkout"
+            "run" (hash
+                "name"    "echo"
+                "command" (string-join '("echo \"hello\"" "uptime") "; ")
+             )
+        )
+   )
+)
+)
